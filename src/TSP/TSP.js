49259502c14b807d7094458e028cbbb0f904b707
Daniel <danielevalberg@gmail.com>
2019-04-20 05:43:35 +0000

    completed tsp and its test

---
+++ src/TSP/TSP.js
@@ -0,0 +1,78 @@
+class TSP {
+    constructor(tour, tempTour, ui, canvas) {
+        this.tour = tour;
+        this.tempTour = tempTour;
+        this.running = false;
+        this.ui = ui;
+        this.canvas = canvas;
+    }
+
+    setupTour(vertices) {
+        this.tour.vertices = vertices;
+        this.tour.updateTourProperties();
+    }
+
+    runSimulation() {
+        if (!this.running) {
+            this.running = true;
+            this.twoOpt();
+        }
+    }
+
+    stopSimulation() {
+        this.running = false;
+    }
+
+    twoOptSwap(i, k) {
+        // take route[0] to route[i - 1] and add them in order to new_route
+        for (let a = 0; a <= i - 1; a++) {
+            this.tempTour.vertices[a] = this.tour.vertices[a];
+        }
+
+        // take route[i] to route[k] and add them in reverse order to new_route
+        let dec = 0;
+        for (let a = i; a <= k; a++) {
+            this.tempTour.vertices[a] = this.tour.vertices[k - dec];
+            dec++;
+        }
+
+        // take route[k+1] to end and add them in order to new_route
+        for (let a = k + 1; a < this.tour.vertices.length; a++) {
+            this.tempTour.vertices[a] = this.tour.vertices[a];
+        }
+
+        this.tempTour.updateTourProperties();
+    }
+
+    async twoOpt() {
+        let improved = true;
+        let generation = 0;
+        this.ui.displayStatus(true);
+        const n = this.tour.vertices.length;
+
+        while (improved && this.running) {
+            improved = false;
+            generation++;
+            for (let i = 1; i < n - 1; i++) {
+                if (!this.running) break;
+                for (let k = i + 1; k < n; k++) {
+                    if (!this.running) break;
+                    this.twoOptSwap(i, k);
+                    this.ui.displayGeneration(generation)
+
+                    if (this.tempTour.totalDistance < this.tour.totalDistance) {
+                        this.tour = Object.assign(Object.create(Object.getPrototypeOf(this.tempTour)), this.tour);
+                        improved = true;
+                    }
+
+                    this.ui.displayTotalDistance(this.tour.totalDistance);
+                    await this.canvas.drawTour(this.tour.vertices, this.tour.edges);
+                }
+            }
+        }
+        this.running = false;
+        this.ui.displayStatus(false);
+    }
+}
+
+export default TSP;
---
+++ src/TSP/TSP.test.js
@@ -0,0 +1,119 @@
+import TSP from './TSP';
+
+let tsp;
+
+class MockTour {
+    constructor() {
+        this.vertices = [];
+        this.tourUpdated = false;
+        this.totalDistance;
+    }
+    updateTourProperties() {
+        this.tourUpdated = true;
+    }
+}
+
+class MockUI {
+    constructor() {
+        this.displayStatusCalled = false;
+        this.displayTotalDistanceCalled = false;
+        this.displayGenerationCalled = false;
+    }
+    displayStatus() {
+        this.displayStatusCalled = true;
+    }
+    displayTotalDistance() {
+        this.displayTotalDistanceCalled = true;
+    }
+    displayGeneration() {
+        this.displayGenerationCalled = true;
+    }
+}
+
+class MockCanvas {
+    constructor() {
+        this.drawTourCalled = false;
+    }
+
+    drawTour() {
+        this.drawTourCalled = true;
+    }
+}
+
+describe("Test setupTour", () => {
+    beforeEach(() => {
+        tsp = new TSP(new MockTour(), new MockTour(), new MockUI(), new MockCanvas());
+    });
+
+    it("Should set up the tour according to vertices given and execute tour's updateTourProperties", () => {
+        tsp.setupTour([1, 2, 3, 4, 5]);
+        expect(tsp.tour.vertices).toEqual([1, 2, 3, 4, 5]);
+        expect(tsp.tour.tourUpdated).toEqual(true);
+    });
+})
+
+describe("Test runSimulation", () => {
+    it("Should set running to true and run twoOpt", () => {
+        tsp.twoOptExecuted = false;
+        tsp.twoOpt = () => { tsp.twoOptExecuted = true }
+        tsp.runSimulation();
+
+        expect(tsp.running).toBe(true);
+        expect(tsp.twoOptExecuted).toBe(true);
+    });
+});
+
+describe("Test stopSimulation", () => {
+    it("Should set running to false", () => {
+        tsp.running = true;
+        tsp.stopSimulation();
+        expect(tsp.running).toBe(false);
+    });
+});
+
+describe("Test twoOptSwap", () => {
+    it("Should swap elements correctly: i = 3, k = 6 and output is set in tempTour. Then execute updateTourProperties in tempTour.", () => {
+        const input = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A'];
+        const output = ['A', 'B', 'C', 'G', 'F', 'E', 'D', 'H', 'A'];
+        tsp.tour.vertices = input;
+        tsp.tempTour.updateExecuted = false;
+        tsp.twoOptSwap(3, 6);
+        expect(tsp.tempTour.vertices).toEqual(output);
+        expect(tsp.tempTour.tourUpdated).toBe(true);
+
+        // check to see if original tour is still the same
+        expect(tsp.tour.vertices).toEqual(input);
+    })
+})
+
+describe("Test twoOpt", () => {
+    beforeEach(() => {
+        tsp = new TSP(new MockTour(), new MockTour(), new MockUI(), new MockCanvas());
+        tsp.setupTour([{ x: 77, y: 12 }, { x: 20, y: 20 }, { x: 50, y: 40 }, { x: 25, y: 10 }, { x: 30, y: 11 }]);
+        tsp.running = true;
+    });
+
+    it("Should call twoOptSwap to swap points", () => {
+        tsp.twoOptSwapCalled = false;
+        tsp.twoOptSwap = (i, j) => { tsp.twoOptSwapCalled = true }
+        tsp.twoOpt();
+        expect(tsp.twoOptSwapCalled).toBe(true);
+    });
+
+    it("Should call its UI displayStatus", () => {
+        tsp.twoOpt();
+        expect(tsp.ui.displayStatusCalled).toBe(true);
+    });
+
+    it("Should call its UI displayTotalDistance", () => {
+        let tourDistance = 0;
+        tsp.twoOptSwap = (i, j) => { tsp.tour.totalDistance = tourDistance++; tsp.tempTour.totalDistance = 5 }
+        tsp.twoOpt();
+        expect(tsp.ui.displayTotalDistanceCalled).toBe(true);
+    });
+
+    it("Should call its UI displayGeneration", () => {
+        tsp.twoOpt();
+        expect(tsp.ui.displayGenerationCalled).toBe(true);
+    });
+});
